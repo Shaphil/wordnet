f80f8b1cbe6d1db938fc3e6e7255b9e415b46ef5
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2021-08-06 18:18:07 +0600

    Add model Senses

--- bangla/models.py
+++ bangla/models.py
@@ -32,3 +32,15 @@ class EnglishWords(models.Model):
 
     def __str__(self):
         return self.word
+
+
+class Senses(models.Model):
+    class Meta:
+        verbose_name_plural = 'Senses'
+
+    sense_id = models.CharField(max_length=32, primary_key=True)
+    synset = models.CharField(max_length=32)
+    eng_id = models.ForeignKey(EnglishWords, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.eng_id.word
