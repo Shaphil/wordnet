667e38247843e1ed27930ab16906e71503010ac0
Md. Abdullah Al Mahmud <shaphil2025@gmail.com>
2021-08-06 18:14:43 +0600

    Add models EnglishWords and BanglaEnglishRelations

--- bangla/models.py
+++ bangla/models.py
@@ -10,3 +10,25 @@ class BanglaWords(models.Model):
 
     def __str__(self):
         return self.bangla_word
+
+
+class BanglaEnglishRelations(models.Model):
+    class Meta:
+        verbose_name_plural = 'Bangla English Relations'
+
+    bng_id = models.OneToOneField(
+        'BanglaWords', primary_key=True, on_delete=models.CASCADE)
+    eng_id = models.ForeignKey('EnglishWords', on_delete=models.CASCADE)
+
+
+class EnglishWords(models.Model):
+    class Meta:
+        verbose_name_plural = 'English Words'
+
+    eng_id = models.IntegerField(primary_key=True)
+    word = models.CharField(max_length=64)
+    bangla_word = models.ManyToManyField(
+        BanglaWords, through=BanglaEnglishRelations)
+
+    def __str__(self):
+        return self.word
